a2a718341e03721daeb4f112dd7366fb5ce7bca9
Алина <koroleevalina@gmail.com>
2023-04-18 00:32:10 +0300

    lab_3

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello ANdrew")
+def task1():
+    n = int(input("Введите количество слов"))
+    snew = ""
+    for i in range(n):
+        slovo = input("Введите слово")
+        snew = snew + " " + slovo
+    print(snew)
+
+
+def task2():
+    snew = ""
+    slovo = (input("Введите слово"))
+    while slovo != ("stop"):
+        slovo = (input("Введите слово"))
+        snew = snew + " " + slovo
+    print(snew)
+
+
+def task3():
+    slovo = (input('Введите слово'))
+    for i in slovo:
+        if (i == "ф"):
+            print("Ого! Это редкое слово!")
+        else:
+            print("Эх, это не очень редкое слово...")
+
+
+task3()
