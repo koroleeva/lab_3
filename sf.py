b3d58c8b4772c0118d2f38a0363ff41abb587852
Алина <koroleevalina@gmail.com>
2023-04-19 14:10:28 +0300

    lab_3

--- sf.py
+++ sf.py
@@ -23,19 +23,3 @@
 #         print(f"Вы проиграли. Количество правильных ответов:{q}")
 #         break
 
-from random import randint
-mist=0
-right=0
-while mist!=3:
-    m = randint(1, 10)
-    n = randint(1, 10)
-    summ=m+n
-    print(m,"+",n)
-    f=int(input())
-    if f == summ:
-        print("Правильно!")
-        right+=1
-    else:
-        mist+=1
-        print("Ответ неверный")
-print("Игра окончена.", "Правильных ответов:", right)
